dc0baea2fd9af6f8771803a27957fa5b73f5621e
Aram <aram_martin@alumni.brown.edu>
2021-07-07 11:08:01 -0500

    ADD: initial commit

---
+++ Inheritance/class.js
@@ -0,0 +1,13 @@
+/*
+Classes are a template for creating objects. They encapsulate data with code to work on that data.
+
+It is often useful to define a class of objects that share certain properties. Members, or instances, of the class have their own properties to hold or define their state, but they also have methods that define their behavior. These methods are defined by the class and shared by all instances.
+
+Properties --- define state
+Methods --- define behavior on state
+
+Classes use prototype-based inheritance: if two objects inherit properties from the same object, then we say those objects are instances of the same class. 
+
+
+
+*/

63fdf717185c2dad32b23d2f1446dd63592828f3
Aram <aram_martin@alumni.brown.edu>
2021-07-09 11:28:19 -0500

    ADD: prototype defintion

---
+++ prototype.js
@@ -0,0 +1,10 @@
+/*
+All objects created by object literals have the same prototype object --- Object.prototype
+
+that is to say, javaScript objects have a set of "own properties," and they also inherit a set of properties from their prototype object.
+
+Almost all objects have a prototype, but only a small number of objects have a protytpe property. These objects with prototype properties define the prototypes for all other objects.
+
+
+
+*/
